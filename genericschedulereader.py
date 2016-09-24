c0c998a91c0fb63f447877b338b154094af7d1a4
trevortds <trevortds3@gmail.com>
2016-09-24 12:17:32 -0700

    microseconds

--- genericschedulereader.py
+++ genericschedulereader.py
@@ -20,8 +20,7 @@ def gt(dt_str):
     '''
     dt, _, us = dt_str.partition(".")
     dt = datetime.datetime.strptime(dt, "%Y-%m-%dT%H:%M:%S")
-    us = int(us.rstrip("Z"), 10)
-    return dt + datetime.timedelta(microseconds=us)
+    return dt
 
 
 def add_event(name, time, text):
