576be84726abdd6c289fb6b7eaf3d6cec1f03b6c
TrevorTDS <trevortds3@gmail.com>
2016-09-24 20:26:24 -0700

    version numder

--- lingbot.py
+++ lingbot.py
@@ -16,7 +16,7 @@ with open("api.txt", 'r') as f:
 BOT_ID = os.environ.get("BOT_ID")
 BOT_ID = "U25Q053D4"
 
-version_number = "0.2.0"
+version_number = "0.2.1"
 
 AT_BOT = "<@" + BOT_ID + ">"
 EXAMPLE_COMMAND = "do"
