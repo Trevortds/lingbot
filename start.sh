20ec784936c97d4a92c31263c8cea0c08bcb64d1
Trevor <trevortds3@gmail.com>
2016-09-28 12:48:23 -0700

    made startup script automatically reboot on error

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-python3 /home/pi/github/lingbot/lingbot.py > log.txt 2> err.txt &
+while true; do
+	if pgrep "python3" > /dev/null
+	then
+		true
+	else
+		python3 /home/pi/github/lingbot/lingbot.py > log.txt 2> err.txt &
+	fi
+done
